8ca2607a576797a2529939a351b0c04bf78219c9
Guillermo Arámburo Rodríguez <garamburor@hotmail.com>
2024-03-17 12:56:19 +0100

    Replaced sawtooth with bandlimited method.

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -19,18 +19,41 @@ public:
     float inc;
     float phase;
 
+    // for BL saw
+    float freq;
+    float sampleRate;
+    float phaseBL;
+
     void reset()
     {
         phase = 0.0f;
+        phaseBL = -0.5f;
+    }
+
+    float nextBandLimitedSample()
+    {
+        phaseBL += inc;
+        if (phaseBL >= 1.0f) {
+            phaseBL -= 1.0f;
+        }
+
+        float output = 0.0f;
+        float nyquist = sampleRate * 0.5f;
+        float h = freq;
+        float i = 1.0f;
+        float m = 0.6366197724f; // 2 / pi
+        while (h < nyquist) { // sum harmonics
+            output += m * std::sin(TWO_PI * phaseBL * i) / i;
+            h += freq;
+            i += 1.0f;
+            m = -m;
+        }
+
+        return output;
     }
 
     float nextSample()
     {
-        phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
-        }
-
-        return amplitude * (2.0f * phase - 1.0f);
+        return amplitude * nextBandLimitedSample();
     }
 };
