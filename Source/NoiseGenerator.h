683bd7d726f8a818e9893e8aa3ad72803297c418
garamburor <garamburor@hotmail.com>
2024-03-17 12:26:29 +0100

    Code up until sine oscillator using digital resonator.

---
+++ Source/NoiseGenerator.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    NoiseGenerator.h
+    Created: 1 Mar 2024 3:12:52pm
+    Author:  garam
+
+  ==============================================================================
+*/
+
+#pragma once
+
+class NoiseGenerator
+{
+public:
+    void reset()
+    {
+        noiseSeed = 22222;
+    }
+
+    float nextValue()
+    {
+        // Generate the next integer pseudorandom number
+        noiseSeed = noiseSeed * 196314165 + 907633515;
+
+        // Convert the integer to a float, to get a number between 2 and 4
+        unsigned int r = (noiseSeed & 0x7FFFFF) + 0x40000000;
+        float noise = *(float*)&r;
+
+        // Substract 3 to get the float into the range (-1,1)
+        return noise - 3.0f;
+    }
+
+private:
+    unsigned int noiseSeed;
+
+};
---
+++ Source/Synth.h
@@ -0,0 +1,35 @@
+/*
+  ==============================================================================
+
+    Synth.h
+    Created: 1 Mar 2024 2:53:03pm
+    Author:  garam
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "Voice.h"
+#include "NoiseGenerator.h"
+
+class Synth
+{
+public:
+    Synth();
+
+    void allocateResources(double sampleRAte, int samplesPerBlock);
+    void deallocateResources();
+    void reset();
+    void render(float** outputBuffers, int sampleCount);
+    void midiMessage(uint8_t data0, uint8_t data1, uint8_t data2);
+
+private:
+    void noteOn(int note, int velocity);
+    void noteOff(int note);
+
+    float sampleRate;
+    Voice voice;
+    NoiseGenerator noiseGen;
+};
