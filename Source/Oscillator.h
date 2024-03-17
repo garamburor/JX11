69ff853d6c45e1ef8621ac8774ee01941a47d47a
Guillermo Arámburo Rodríguez <garamburor@hotmail.com>
2024-03-17 13:18:33 +0100

    Changed output to BLIT saw. Has a DC component.

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -10,55 +10,59 @@
 
 #pragma once
 
+
+const float PI_OVER_4 = 0.7853981633974483f;
+const float PI = 3.1415926535897932f;
 const float TWO_PI = 6.2831853071795864f;
 
 class Oscillator
 {
 public:
+    float period = 0.0f;
     float amplitude;
-    float inc;
-    float phase;
-
-    // for BL saw
-    float freq;
-    float sampleRate;
-    float phaseBL;
 
     void reset()
     {
+        inc = 0.0f;
         phase = 0.0f;
-        phaseBL = -0.5f;
-    }
-
-    float nextBandLimitedSample()
-    {
-        phaseBL += inc;
-        if (phaseBL >= 1.0f) {
-            phaseBL -= 1.0f;
-        }
-
-        float output = 0.0f;
-        float nyquist = sampleRate * 0.5f;
-        float h = freq;
-        float i = 1.0f;
-        float m = 0.6366197724f; // 2 / pi
-        while (h < nyquist) { // sum harmonics
-            output += m * std::sin(TWO_PI * phaseBL * i) / i;
-            h += freq;
-            i += 1.0f;
-            m = -m;
-        }
-
-        return output;
     }
 
     float nextSample()
     {
-        phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
+        float output = 0.0f;
+
+        phase += inc; // 1
+        if (phase <= PI-PI_OVER_4) { // 2
+            // 3
+            float halfPeriod = period * 0.5f;
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            phaseMax += PI;
+
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+
+            // 4
+            if (phase * phase > 1e-9) {
+                output = amplitude * std::sin(phase) / phase;
+            }
+            else {
+                output = amplitude;
+            }
         }
-        float aliased = 2.0f * phase - 1.0f;
-        return amplitude * (aliased -  nextBandLimitedSample());
+        else { // 5
+            // 6
+            if (phase > phaseMax) {
+                phase = phaseMax + phaseMax - phase;
+                inc = -inc;
+            }
+            // 7
+            output = amplitude * std::sin(phase);
+        }
+
+        return output;
     }
+private:
+    float phase;
+    float phaseMax;
+    float inc;
 };
--- Source/Synth.cpp
+++ Source/Synth.cpp
@@ -86,9 +86,7 @@ void Synth::noteOn(int note, int velocity)
     voice.note = note;
     float freq = 440.0f * std::exp2(float(note - 69) / 12.0f);
     voice.osc.amplitude = (velocity / 127.0f) * 0.5f;
-    voice.osc.inc = freq / sampleRate;
-    voice.osc.freq = freq;
-    voice.osc.sampleRate = sampleRate;
+    voice.osc.period = sampleRate / freq;
     voice.osc.reset();
 }
 
